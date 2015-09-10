963a6e817bdeba4fba3d46349a5b7028492b4d64
maharjun <maharjun@thoughtworks.com>
2015-09-10 11:46:24 +0530

    Added spec in Parser for returning item rate and tax percent for non-imported book items

--- src/com/twu/baselineproblem/Parser.java
+++ src/com/twu/baselineproblem/Parser.java
@@ -13,4 +13,12 @@ public class Parser {
     public String[] split() {
         return item.split(" ");
     }
+
+    public double[] parse() {
+        String[] list = split();
+        double[] resultArray = new double[2];
+        resultArray[0] = Double.parseDouble(list[list.length-1]);
+        resultArray[1] = 0.0;
+        return resultArray;
+    }
 }
--- test/com/twu/baselineproblem/ParserTest.java
+++ test/com/twu/baselineproblem/ParserTest.java
@@ -7,12 +7,21 @@ import static org.junit.Assert.*;
 public class ParserTest {
 
     @Test
-    public void shouldRuturnTheListWhenWePassString(){
-        String input ="1 book at 12.49";
+    public void shouldRuturnTheListWhenWePassString() {
+        String input = "1 book at 12.49";
         Parser parser = new Parser(input);
         String[] result = input.split(" ");
 
         assertArrayEquals(result, parser.split());
+    }
+
+    @Test
+    public void shouldRuturnTheItemRateAndTaxPercentWhenItemIsNotImported() {
+        String input = "1 book at 12.49";
+        Parser parser = new Parser(input);
+        double result[] = {12.49, 0.0};
+
+        assertArrayEquals(parser.parse(), result, 0.00);
     }
 
 }
